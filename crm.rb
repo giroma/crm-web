1f0379a1a3f88d17c5ab178173012789ee622775
Argir Argirov <dj.giro@gmail.com>
2017-10-05 15:44:19 -0400

    form does things now!

--- crm.rb
+++ crm.rb
@@ -18,6 +18,17 @@ get '/add_contact' do
   erb :add_contact
 end
 
+post '/add_contact' do
+  Contact.create(
+    first_name: params[:first_name],
+    last_name: params[:last_name],
+    email: params[:email],
+    note: params[:note]
+  )
+
+  redirect to('/')
+end
+
 get '/contact/:id' do
   begin
     @contact = Contact.find(params[:id])
